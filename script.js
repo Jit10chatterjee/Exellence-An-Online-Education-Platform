b70c174651313bd4ed1fd13c2641ef7ce9aab9d5
Jit10chatterjee <chatterjeejit10@gmail.com>
2023-12-08 20:55:33 +0530

    change

--- script.js
+++ script.js
@@ -38,10 +38,10 @@ $(document).ready(function () {
         });
     });
 });
-function loader() {
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
-function fadeOut() {
-    setInterval(loader, 2000);
-}
-window.onload = fadeOut;
+// function loader() {
+//     document.querySelector('.loader-container').classList.add('fade-out');
+// }
+// function fadeOut() {
+//     setInterval(loader, 2000);
+// }
+// window.onload = fadeOut;
